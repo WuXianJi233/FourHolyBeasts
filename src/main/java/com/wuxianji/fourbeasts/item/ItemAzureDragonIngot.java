401e18d66f306ff7ccdc732d451ff348069aece1
wuxianji <1067037318@QQ.com>
2023-10-30 17:54:47 +0800

    创建青龙锭

---
+++ src/main/java/com/wuxianji/fourbeasts/item/ItemAzureDragonIngot.java
@@ -0,0 +1,11 @@
+package com.wuxianji.fourbeasts.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class ItemAzureDragonIngot extends Item {
+
+    ItemAzureDragonIngot(){
+        super(new Properties().group(ItemGroup.TOOLS));
+    }
+}
