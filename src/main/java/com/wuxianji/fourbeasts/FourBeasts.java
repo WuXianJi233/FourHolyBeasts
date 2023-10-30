22a36b78805c6e0eed29b4a496f7f1f634de7247
wuxianji <1067037318@QQ.com>
2023-10-30 17:25:41 +0800

    第一次提交项目src

---
+++ src/main/java/com/wuxianji/fourbeasts/FourBeasts.java
@@ -0,0 +1,8 @@
+package com.wuxianji.fourbeasts;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Utils.MOD_ID)
+public class FourBeasts {
+
+}
---
+++ src/main/java/com/wuxianji/fourbeasts/Utils.java
@@ -0,0 +1,6 @@
+package com.wuxianji.fourbeasts;
+
+public class Utils {
+
+    public static final String MOD_ID = "fourbeasts";
+}
